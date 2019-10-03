296ee51421d3b0fb4cdb716f27e567b514becd27
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 17:57:36 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,20 @@ def map(array)
   counter = 0
   while counter < array.length do
     new_array.push(yield(array[counter]))
-    counter+=
+    counter+= 1
   end
   new_array
+end
+
+def reduce(array, starting_point=0)
+  sum = starting_point
+  counter = 0
+  while counter < array.length do
+    sum = (yield(array[counter], sum))
+      if !sum
+        return false
+      end
+    counter+= 1
+  end
+  sum
 end
