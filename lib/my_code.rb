edfc9d4daa19130182b7066f6110aef9c51640b9
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 17:30:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array.push(yield(array[counter]))
+    counter+=
+  end
+  new_array
+end
